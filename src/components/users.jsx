2a0e60ee6969cbc10d1e20678d7ea08a9db6032f
Grechkin Danil <grechkin-danil@mail.ru>
2022-09-18 18:16:24 +0300

    finish refactoring

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,76 +1,59 @@
 import React, { useState } from 'react';
+import { User } from './user';
 import api from '../api';
+import { Status } from './searchStatus';
+
 export const Users = () => {
-  // состояние (массив из объектов)
   const [getUsers, setUsers] = useState(api.users.fetchAll());
-  const deletUser = (id) => {
-    setUsers((prevState) => prevState.filter((user) => user._id !== id));
-  };
-  // функция основного рендера, в котором в <tbody> вызыывается массив из состояния, измененный на разметку с подставленными значениями
-  const createPatternTable = (value, text, classNameTitle) => {
-    return (
-      <>
-        <h1 className={classNameTitle}>{`${value} ${text}`}</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Професия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-            </tr>
-          </thead>
-          <tbody>
-            {getUsers.map((user) => {
-              return (
-                <tr key={user._id} className={user._id}>
-                  <th>{user.name}</th>
-                  <th>
-                    {user.qualities.map((qual) => (
-                      <span
-                        key={qual._id}
-                        className={`badge bg-${qual.color} m-1`}
-                      >
-                        {qual.name}
-                      </span>
-                    ))}
-                  </th>
-                  <th>{user.profession.name}</th>
-                  <th>{user.completedMeetings}</th>
-                  <th>{`${user.rate}/5`}</th>
-                  <button
-                    className="badge bg-danger mt-2"
-                    onClick={() => deletUser(user._id)}
-                  >
-                    Delete
-                  </button>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
+  // метод, который удаляет юзеров
+  // этот метод вызывается в пропсе в компоненте User
+  const handleDelete = (userId) => {
+    setUsers(
+      getUsers.filter((item) => {
+        return item._id !== userId;
+      })
     );
   };
-  if (
-    getUsers.length === 4 ||
-    getUsers.length === 3 ||
-    getUsers.length === 2 ||
-    getUsers.length === 1
-  ) {
-    return createPatternTable(
-      getUsers.length,
-      'Человека тусанет с тобой сегодня',
-      'badge bg-primary'
-    );
-  }
-  if (getUsers.length === 0) {
-    return <h1 className="badge bg-danger">С тобой никто не тусанет</h1>;
-  }
-  return createPatternTable(
-    getUsers.length,
-    'Человек тусанут с тобой сегодня',
-    'badge bg-primary'
+  // функция, в которой меняется состояние ключа bookmark
+  // при нажатии на кнопку, вычисляется индекс, потом создается новый объект, где по указаному индксу в ключе bookmark меняется значение
+  const handleMark = (state, id) => {
+    const indexBookMark = getUsers.findIndex((item) => {
+      return item._id === id;
+    });
+    const newState = [...getUsers];
+    if (newState[indexBookMark].bookmark === false) {
+      newState[indexBookMark].bookmark = true;
+    } else if (newState[indexBookMark].bookmark === true) {
+      newState[indexBookMark].bookmark = false;
+    }
+    setUsers(newState);
+  };
+  return (
+    <>
+      <Status value={getUsers} />
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Професия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getUsers.map((item) => {
+            return (
+              <User
+                key={item._id}
+                {...item}
+                onDelete={handleDelete}
+                onMark={handleMark}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Users } from './components/testUsers';
-import { TestUsers } from './components/testUsers';
+import { Users } from './components/users';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<TestUsers />);
+root.render(<Users />);
