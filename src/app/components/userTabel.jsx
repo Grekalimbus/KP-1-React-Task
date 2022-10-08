ea4fb0982ec2e4612fe5684c1b0997b0faeffd89
Grechkin Danil <grechkin-danil@mail.ru>
2022-10-08 19:57:22 +0300

    ready userTabel

--- src/app/components/userTabel.jsx
+++ src/app/components/userTabel.jsx
@@ -2,17 +2,43 @@ import React from "react";
 import User from "./user";
 import PropTypes from "prop-types";
 
-const UserTabel = ({ users, onDelete, onMark }) => {
+const UserTabel = ({ users, onDelete, onMark, currentSort, onSort }) => {
+    const handleSort = (item) => {
+        if (currentSort.iter === item) {
+            onSort({
+                ...currentSort,
+                order: currentSort.order === "asc" ? "desc" : "asc"
+            });
+        } else {
+            onSort({ iter: item, order: "asc" });
+        }
+    };
     return (
         <table className="table">
             <thead>
                 <tr>
-                    <th scope="col">Имя</th>
+                    <th onClick={() => handleSort("name")} scope="col">
+                        Имя
+                    </th>
                     <th scope="col">Качества</th>
-                    <th scope="col">Професия</th>
-                    <th scope="col">Встретился, раз</th>
-                    <th scope="col">Оценка</th>
-                    <th scope="col"></th>
+                    <th
+                        onClick={() => handleSort("profession.name")}
+                        scope="col"
+                    >
+                        Професия
+                    </th>
+                    <th
+                        onClick={() => handleSort("completedMeetings")}
+                        scope="col"
+                    >
+                        Встретился, раз
+                    </th>
+                    <th onClick={() => handleSort("rate")} scope="col">
+                        Оценка
+                    </th>
+                    <th onClick={() => handleSort("bookmark")} scope="col">
+                        Избранное
+                    </th>
                     <th scope="col"></th>
                 </tr>
             </thead>
@@ -34,6 +60,8 @@ const UserTabel = ({ users, onDelete, onMark }) => {
 UserTabel.propTypes = {
     users: PropTypes.array.isRequired,
     onDelete: PropTypes.func.isRequired,
-    onMark: PropTypes.func.isRequired
+    onMark: PropTypes.func.isRequired,
+    onSort: PropTypes.func.isRequired,
+    currentSort: PropTypes.object.isRequired
 };
 export default UserTabel;
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -6,27 +6,40 @@ import { Paginate } from "../utils/paginate";
 import PropTypes from "prop-types";
 import GroupList from "./groupList";
 import api from "../api";
+import _ from "lodash";
 
 export const Users = (props) => {
     // users - массив из объектов (12)
     // metodDelet/metodHandleMark - пропсы, в которых методы из файла App.js
     const { users, methodDelete, methodHandleMark } = props;
 
-    // 2 константы для работы с копмонентом pagination, эти константы будут использованы как пропсы
-
-    const pageSize = 2; // количество пользователей, которые будут отображатся на странице
+    const pageSize = 8; // количество пользователей, которые будут отображатся на странице
+    // состояния
     const [currentPage, setCurrenPage] = useState(1);
     // professions - массив из объектов профессий (6шт) / [{},{},...]
     const [professions, setProffesion] = useState();
     // selectedProf - объект одной из професии / { _id: "67rdca3eeb7f6fgeed471818", name: "Доктор" }
     const [selectedProf, setSelectedProf] = useState();
+    // состояние для сортировки
+    const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" });
+    // методы
+    // метод для пагинации
     const handlePageChange = (pageIndex) => {
         console.log("page: ", pageIndex);
         setCurrenPage(pageIndex);
     };
+    // метод для груп листа
     const handleProfessionSelect = (item) => {
         setSelectedProf(item);
         setCurrenPage(1);
+    };
+    // метод для сбрасывания состояния selectedProf
+    const clearfilter = () => {
+        setSelectedProf();
+    };
+    // метод для таблицы
+    const handleSort = (item) => {
+        setSortBy(item);
     };
     // хук, который обрабатывает промис и получает данные, после чего вызывается setProffesion, чтобы установить состояние professions
     // это происходит так потому, что данные приходят не сразу, поэтому состояние устанавливается в этом хуке после того, как пришли данные
@@ -40,11 +53,10 @@ export const Users = (props) => {
         ? users.filter((user) => user.profession._id === selectedProf._id)
         : users;
     const count = filteredUsers.length;
-    const userCrop = Paginate(filteredUsers, currentPage, pageSize); // Paginate - функция которая режет массив обьекта и оставляет там 4 элемента
-    // начиная с полученного индекса
-    const clearfilter = () => {
-        setSelectedProf();
-    };
+    // сортировка с помощью lodash
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order]);
+    const userCrop = Paginate(sortedUsers, currentPage, pageSize); // Paginate - функция которая режет массив обьекта и оставляет там 4 элемента
+
     return (
         <div className="d-flex">
             {professions && (
@@ -68,6 +80,8 @@ export const Users = (props) => {
                     users={userCrop}
                     onMark={methodHandleMark}
                     onDelete={methodDelete}
+                    onSort={handleSort}
+                    currentSort={sortBy}
                 />
                 <div className="d-flex justify-content-center">
                     <Pagination
