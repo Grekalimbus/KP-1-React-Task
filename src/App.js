fa080c1d58e74886781d5a5d8f3a14ee6400013a
Grechkin Danil <grechkin-danil@mail.ru>
2022-10-11 18:28:10 +0300

    finish refactoring

--- src/App.js
+++ src/App.js
@@ -1,46 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Users } from "./app/components/users";
-import api from "./app/api";
 
 export function App() {
-    const [getUsers, setUsers] = useState();
-    useEffect(() => {
-        api.users.fetchAll().then((users) => {
-            return setUsers(users);
-        });
-    }, []);
-    // метод, который удаляет юзеров
-    // этот метод вызывается в пропсе в компоненте User
-    const handleDelete = (userId) => {
-        setUsers(
-            getUsers.filter((item) => {
-                return item._id !== userId;
-            })
-        );
-    };
-    // функция, в которой меняется состояние ключа bookmark
-    // при нажатии на кнопку, вычисляется индекс, потом создается новый объект, где по указаному индксу в ключе bookmark меняется значение
-    const handleMark = (state, id) => {
-        const indexBookMark = getUsers.findIndex((item) => {
-            return item._id === id;
-        });
-        const newState = [...getUsers];
-        if (newState[indexBookMark].bookmark === false) {
-            newState[indexBookMark].bookmark = true;
-        } else if (newState[indexBookMark].bookmark === true) {
-            newState[indexBookMark].bookmark = false;
-        }
-        setUsers(newState);
-    };
-    if (getUsers) {
-        return (
-            <div>
-                <Users
-                    methodDelete={handleDelete}
-                    methodHandleMark={handleMark}
-                    users={[...getUsers]}
-                />
-            </div>
-        );
-    }
+    return <Users />;
 }
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -3,25 +3,19 @@ import UserTabel from "./userTabel";
 import Status from "./searchStatus";
 import Pagination from "./pagination";
 import { Paginate } from "../utils/paginate";
-import PropTypes from "prop-types";
+
 import GroupList from "./groupList";
 import api from "../api";
 import _ from "lodash";
 
-export const Users = (props) => {
-    // users - массив из объектов (12)
-    // metodDelet/metodHandleMark - пропсы, в которых методы из файла App.js
-    const { users, methodDelete, methodHandleMark } = props;
-
+export const Users = () => {
     const pageSize = 8; // количество пользователей, которые будут отображатся на странице
     // состояния
     const [currentPage, setCurrenPage] = useState(1);
-    // professions - массив из объектов профессий (6шт) / [{},{},...]
     const [professions, setProffesion] = useState();
-    // selectedProf - объект одной из професии / { _id: "67rdca3eeb7f6fgeed471818", name: "Доктор" }
     const [selectedProf, setSelectedProf] = useState();
-    // состояние для сортировки
     const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" });
+    const [getUsers, setUsers] = useState();
     // методы
     // метод для пагинации
     const handlePageChange = (pageIndex) => {
@@ -41,6 +35,35 @@ export const Users = (props) => {
     const handleSort = (item) => {
         setSortBy(item);
     };
+    // console.log(getUsers);
+    useEffect(() => {
+        api.users.fetchAll().then((users) => {
+            return setUsers(users);
+        });
+    }, []);
+    // метод, который удаляет юзеров
+    // этот метод вызывается в пропсе в компоненте User
+    const handleDelete = (userId) => {
+        setUsers(
+            getUsers.filter((item) => {
+                return item._id !== userId;
+            })
+        );
+    };
+    // функция, в которой меняется состояние ключа bookmark
+    // при нажатии на кнопку, вычисляется индекс, потом создается новый объект, где по указаному индксу в ключе bookmark меняется значение
+    const handleMark = (state, id) => {
+        const indexBookMark = getUsers.findIndex((item) => {
+            return item._id === id;
+        });
+        const newState = [...getUsers];
+        if (newState[indexBookMark].bookmark === false) {
+            newState[indexBookMark].bookmark = true;
+        } else if (newState[indexBookMark].bookmark === true) {
+            newState[indexBookMark].bookmark = false;
+        }
+        setUsers(newState);
+    };
     // хук, который обрабатывает промис и получает данные, после чего вызывается setProffesion, чтобы установить состояние professions
     // это происходит так потому, что данные приходят не сразу, поэтому состояние устанавливается в этом хуке после того, как пришли данные
     useEffect(() => {
@@ -48,55 +71,58 @@ export const Users = (props) => {
             return setProffesion(data);
         });
     }, []);
+    if (getUsers !== undefined) {
+        const filteredUsers = selectedProf
+            ? getUsers.filter(
+                  (user) => user.profession._id === selectedProf._id
+              )
+            : getUsers;
+        const count = filteredUsers.length;
+        // сортировка с помощью lodash
+        const sortedUsers = _.orderBy(
+            filteredUsers,
+            [sortBy.path],
+            [sortBy.order]
+        );
+        const userCrop = Paginate(sortedUsers, currentPage, pageSize); // Paginate - функция которая режет массив обьекта и оставляет там 4 элемента
 
-    const filteredUsers = selectedProf
-        ? users.filter((user) => user.profession._id === selectedProf._id)
-        : users;
-    const count = filteredUsers.length;
-    // сортировка с помощью lodash
-    const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order]);
-    const userCrop = Paginate(sortedUsers, currentPage, pageSize); // Paginate - функция которая режет массив обьекта и оставляет там 4 элемента
-
-    return (
-        <div className="d-flex">
-            {professions && (
-                <div className="d-flex flex-column flex-shrink-0 p-3">
-                    <GroupList
-                        selectedItem={selectedProf}
-                        items={professions}
-                        onItemSelect={handleProfessionSelect}
+        return (
+            <div className="d-flex">
+                {professions && (
+                    <div className="d-flex flex-column flex-shrink-0 p-3">
+                        <GroupList
+                            selectedItem={selectedProf}
+                            items={professions}
+                            onItemSelect={handleProfessionSelect}
+                        />
+                        <button
+                            className="btn btn-secondary mt-2"
+                            onClick={clearfilter}
+                        >
+                            Очистить
+                        </button>
+                    </div>
+                )}
+                <div className="d-flex flex-column">
+                    <Status value={getUsers} />
+                    <UserTabel
+                        users={userCrop}
+                        onMark={handleMark}
+                        onDelete={handleDelete}
+                        onSort={handleSort}
+                        selectedSort={sortBy}
                     />
-                    <button
-                        className="btn btn-secondary mt-2"
-                        onClick={clearfilter}
-                    >
-                        Очистить
-                    </button>
-                </div>
-            )}
-            <div className="d-flex flex-column">
-                <Status value={users} />
-                <UserTabel
-                    users={userCrop}
-                    onMark={methodHandleMark}
-                    onDelete={methodDelete}
-                    onSort={handleSort}
-                    selectedSort={sortBy}
-                />
-                <div className="d-flex justify-content-center">
-                    <Pagination
-                        itemsCount={count}
-                        pageSize={pageSize}
-                        currentPage={currentPage}
-                        onPageChange={handlePageChange}
-                    />
+                    <div className="d-flex justify-content-center">
+                        <Pagination
+                            itemsCount={count}
+                            pageSize={pageSize}
+                            currentPage={currentPage}
+                            onPageChange={handlePageChange}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
-Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    methodDelete: PropTypes.func.isRequired,
-    methodHandleMark: PropTypes.func.isRequired
+        );
+    }
+    return "loading...";
 };
