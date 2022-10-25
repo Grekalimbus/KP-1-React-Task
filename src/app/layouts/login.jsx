9c35f1cc1e2469afc883e2ee56a1fcacaeeb2fcd
Grechkin Danil <grechkin-danil@mail.ru>
2022-10-26 02:45:33 +0300

    add state

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 
 const Login = () => {
+    const [data, setData] = useState({ email: "", password: "" });
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+        console.log(target.name);
+    };
     return (
         <form>
+            {/* контролируемое поле */}
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email"></input>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                />
             </div>
+            {/* некотнролируемое поле */}
             <div>
                 <label htmlFor="password">Password </label>
-                <input type="password" id="password"></input>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
             </div>
         </form>
     );
