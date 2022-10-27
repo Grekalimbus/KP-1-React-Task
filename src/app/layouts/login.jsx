03cd1e7dc46f5b2ccaa8eb827f05c4c277383606
Grechkin Danil <grechkin-danil@mail.ru>
2022-10-27 19:21:23 +0300

    add validator

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,23 +1,50 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import TextField from "../components/textField";
+import { validator } from "../utils/validator";
 
 const Login = () => {
     const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
-        console.log(target.name);
+    };
+
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            }
+        },
+        password: {
+            isRequired: { message: "Пороль обязателен для заполнения" }
+        }
     };
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextField
                 id="email"
                 name="email"
                 label="Электронная почта"
                 value={data.email}
                 onChange={handleChange}
+                error={errors.email}
             />
             <TextField
                 type="password"
@@ -26,7 +53,9 @@ const Login = () => {
                 label="Пароль"
                 value={data.password}
                 onChange={handleChange}
+                error={errors.password}
             />
+            <button type="submit">Submit</button>
         </form>
     );
 };
