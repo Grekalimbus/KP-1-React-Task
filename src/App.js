52c898bea56a68a8bbc50ecec7e5724b319f239c
Grechkin Danil <grechkin-danil@mail.ru>
2022-10-29 03:29:30 +0300

    add registerForm

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export function App() {
             <Switch>
                 <Route path="/users/:userId?" component={Users} />
                 <Route path="/main" component={MainPage} />
-                <Route path="/login" component={Login} />
+                <Route path="/login/:type?" component={Login} />
             </Switch>
         </div>
     );
